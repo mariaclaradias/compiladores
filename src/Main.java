b12aedf3421434a4e3d901e063324baead7de97f
Jorge Oliveira <jorgeallancastro@icloud.com>
2019-09-29 17:53:07 -0300

    Correção de erros na leitura de arquivo

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,35 @@ import java.io.FileReader;
 import java.io.InputStreamReader;
 
 public class Main {
-  BufferedReader arquivo;
+  static BufferedReader arquivo;
 
   public static void main(String[] args) {
-    SymbolTable tabela = new SymbolTable();
+    leituraArquivo();
+    // SymbolTable tabela = new SymbolTable();
 
-    tabela.insertID("idade", (byte) 1);
-    tabela.insertID("nome", (byte) 3);
+    // tabela.insertID("idade", (byte) 1);
+    // tabela.insertID("nome", (byte) 3);
 
-    tabela.showSymbolsTable();
+    // tabela.showSymbolsTable();
 
-    Symbol simbolo = tabela.searchLexeme("esse");
-    System.out
-        .println("Token: " + simbolo.getToken() + ", Lexema: " + simbolo.getLexeme() + ", tipo: " + simbolo.getType());
+    // Symbol simbolo = tabela.searchLexeme("esse");
+    // System.out
+    // .println("Token: " + simbolo.getToken() + ", Lexema: " + simbolo.getLexeme()
+    // + ", tipo: " + simbolo.getType());
   }
 
-  public void leituraArquivo() {
+  public static void leituraArquivo() {
     BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
     String nome = "";
 
     try {
       System.out.print("Digite o nome do arquivo a ser lido: ");
-      nomeArquivo = entrada.readLine();
+      nome = entrada.readLine();
 
       if (nome.length() > 0 && nome.charAt(nome.length() - 2) != '.' && nome.charAt(nome.length() - 1) != 'l') {
         System.err.println("Esse arquivo não é válido");
       } else {
-        arquivo = new BufferedReader(new FileReader(file));
+        arquivo = new BufferedReader(new FileReader(nome));
       }
     } catch (Exception e) {
       System.err.println("Arquivo não encontrado");
